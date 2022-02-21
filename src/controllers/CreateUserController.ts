0480bd7623f5eb8fd14420456e6327a9cf3dc742
HenriqueBros <hmbroseghini@hotmail.com>
2022-02-21 18:08:05 -0300

    api_node_TypeORM_inicio

---
+++ src/controllers/CreateUserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response} from 'express';
+import { CreateUserService } from '../services/CreateUserServer';
+class CreateUserController {
+  handle(request: Request ,response: Response) {
+
+    const createUserService = new CreateUserService();
+
+    const name = request.body.name;
+    const email = request.body.email;
+
+    if(name.length === 0 || email.length === 0) {
+      return response.status(400).json({mensagem: 'Informe todos os campos'})
+    }
+
+    const user = createUserService.execute({name, email});
+
+    return response.status(201).json({ user})
+  }
+}
+
+export { CreateUserController }
